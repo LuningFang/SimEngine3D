c1cf52f8f8a49b3250b18f7e1a0616f6765ab76e
LuningFang <lfang9@wisc.edu>
2016-10-13 13:41:44 -0400

    Add files via upload

--- readInput.m
+++ readInput.m
@@ -1,13 +1,60 @@
-%function [bodies, constraints] = readInput(filename)
+function [bodies, constraints] = readInput(filename)
 % open input file
-fid = fopen('pendulum.mdl');
-% TODO: handle ground
-% reading body parts and assign markers
+
+% read body parts and assign attributes
+while 1
+    tline = fgetl(fid);
+    if strcmp(tline, '    "bodies": [')
+        break;
+    end
+end
+
+bodies{1}{1} = 'haha';
+
+while 1
+    if strcmp(tline, '    ],')
+        break;
+    end
+    
+    while 1
+        if strcmp(tline, '        },')
+            break;
+        end
+        tline = fgetl(fid);
+        if (length(findstr(tline,'name'))>0)
+            loc = findstr(tline,'"');
+            name = tline(loc(3)+1:loc(4)-1);
+            % find body ID
+            tline = fgetl(fid);
+            loc_c = findstr(tline,':');
+            id = str2num(tline(loc_c+1:end-1));
+            bodies{id}{1} = name;
+            
+            % find body mass
+            tline = fgetl(fid);
+            loc_c = findstr(tline,':');
+            mass = str2double(tline(loc_c+1:end-1));
+            bodies{id}{2} = mass;
+            
+            % find inertia, position, euler parameter and omega
+            for i = 3:6
+                tline = fgetl(fid);
+                loc1 = findstr(tline,'[');
+                loc2 = findstr(tline,']');
+                val = str2num(tline(loc1:loc2));
+                bodies{id}{i} = val;
+            end
+        end
+    end
+    tline = fgetl(fid);
+end
+
 while 1
     tline = fgetl(fid);
     if strcmp(tline, '    "constraints": [')
         break;
     end
+    
 end
 
 constraints{1}{1} = 'haha';
@@ -16,7 +63,7 @@ while 1
     if strcmp(tline, '    ]')
         break;
     end
-%    tline = fgetl(fid);
+    %    tline = fgetl(fid);
     while 1
         if strcmp(tline, '        },')
             break;
@@ -32,13 +79,13 @@ while 1
             id = str2num(tline(loc_c+1:end-1));
             
             constraints{id}{1} = name;
-
+            
             % find body i number
             tline = fgetl(fid);
             loc_c = findstr(tline,':');
             bodyi = str2num(tline(loc_c+1:end-1));
             constraints{id}{2} = bodyi;
-
+            
             % find body j number
             tline = fgetl(fid);
             loc_c = findstr(tline,':');
@@ -59,12 +106,12 @@ while 1
                 loc1 = findstr(tline,'[');
                 loc2 = findstr(tline,']');
                 val1 = str2num(tline(loc1:loc2));
-
+                
                 tline = fgetl(fid);
                 loc1 = findstr(tline,'[');
                 loc2 = findstr(tline,']');
                 val2 = str2num(tline(loc1:loc2));
-
+                
                 tline = fgetl(fid);
                 loc1 = findstr(tline,'[');
                 loc2 = findstr(tline,']');
@@ -73,7 +120,7 @@ while 1
                 attr = struct(field1, val1, field2, val2, field3, val3);
                 constraints{id}{4} = attr;
             end
-
+            
             if (strcmp(type,'CD'))
                 field1 = 'sPi';
                 field2 = 'sQj';
@@ -82,12 +129,12 @@ while 1
                 loc1 = findstr(tline,'[');
                 loc2 = findstr(tline,']');
                 val1 = str2num(tline(loc1:loc2));
-
+                
                 tline = fgetl(fid);
                 loc1 = findstr(tline,'[');
                 loc2 = findstr(tline,']');
                 val2 = str2num(tline(loc1:loc2));
-
+                
                 tline = fgetl(fid);
                 loc1 = findstr(tline,'[');
                 loc2 = findstr(tline,']');
